ed9a19a20f57960fcbddf6e136552ab7b58a12d5
shifeng007 <shifeng007@gmail.com>
2018-06-19 13:03:27 +0800

    no message

---
+++ util/Constants.py
@@ -0,0 +1,15 @@
+APP_ANDROID = "APP_ANDROID"
+APP_IOS = "APP_IOS"
+ALI_SMALL = "ALI_SMALL"
+ALI_ALIPAY = "ALI_ALIPAY"
+PC_CLIENT = "PC_CLIENT"
+WX_PUBLIC = "WX_PUBLIIC"
+WX_SMALL = "WX_SMALL"
+
+CLIENT = [(APP_ANDROID,"android"),
+          (APP_IOS,"ios"),
+          (ALI_SMALL,u"支付宝小程序"),
+          (ALI_ALIPAY,u"支付宝APP"),
+          (WX_PUBLIC,u"微信公众号"),
+          (WX_SMALL,u"微信小程序"),
+          (PC_CLIENT,u"pc端")]
